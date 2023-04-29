f7972ebc29a8b496ade2bcf99593cb83cb38a2b6
cebor <felix@stkn.org>
2023-04-29 02:31:12 +0200

    add cache

--- app/main.py
+++ app/main.py
@@ -3,10 +3,18 @@ from app.blaue_tonne import get_dates
 
 app = FastAPI()
 
+cache = {
+    "lk_rosenheim": {}
+}
+
 
 @app.get("/lk_rosenheim")
 async def blaue_tonne_dates(district):
     URL = "https://chiemgau-recycling.de/wp-content/uploads/2022/11/Abfuhrplan_LK_Rosenheim_2023.pdf"
     PAGES = "1,2"
 
-    return list(get_dates(URL, PAGES, district))
+    if district in cache["lk_rosenheim"]:
+        return cache["lk_rosenheim"][district]
+
+    cache["lk_rosenheim"][district] = list(get_dates(URL, PAGES, district))
+    return cache["lk_rosenheim"][district]
