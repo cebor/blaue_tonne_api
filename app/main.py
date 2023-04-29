e2dcbe82c1885a136a7bca7a6ec33edce741cc50
cebor <felix@stkn.org>
2023-04-29 03:24:37 +0200

    fix docs

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ cache = {
 
 
 @app.get("/lk_rosenheim")
-async def blaue_tonne_dates(district) -> list[str]:
+async def blaue_tonne_dates(district: str) -> list[str]:
     URL = "https://chiemgau-recycling.de/wp-content/uploads/2022/11/Abfuhrplan_LK_Rosenheim_2023.pdf"
     PAGES = "1,2"
 
