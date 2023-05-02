e467779ee7529b9b94f71f4db23bae6660901f20
cebor <felix@stkn.org>
2023-05-02 13:05:08 +0200

    use constants

--- app/main.py
+++ app/main.py
@@ -4,22 +4,24 @@ from app.blaue_tonne import DistrictNotFoundException, get_dates
 
 app = FastAPI()
 
+LANDKREIS = "lk_rosenheim"
+
 cache = {
-    "lk_rosenheim": {}
+    LANDKREIS: {}
 }
 
 
-@app.get("/lk_rosenheim")
+@app.get("/" + LANDKREIS)
 async def blaue_tonne_dates(district: str) -> list[str]:
     URL = "https://chiemgau-recycling.de/wp-content/uploads/2022/11/Abfuhrplan_LK_Rosenheim_2023.pdf"
     PAGES = "1,2"
 
-    if district in cache["lk_rosenheim"]:
-        return cache["lk_rosenheim"][district]
+    if district in cache[LANDKREIS]:
+        return cache[LANDKREIS][district]
 
     try:
-        cache["lk_rosenheim"][district] = list(get_dates(URL, PAGES, district))
+        cache[LANDKREIS][district] = list(get_dates(URL, PAGES, district))
     except DistrictNotFoundException:
         raise HTTPException(status_code=404, detail="District not found")
 
-    return cache["lk_rosenheim"][district]
+    return cache[LANDKREIS][district]
