7adf1afac781ee35fe367e6bca42c907afaf5fb0
Felix Itzenplitz <felix@stkn.org>
2025-02-06 16:13:36 +0100

    format code

--- app/main.py
+++ app/main.py
@@ -6,17 +6,24 @@ app = FastAPI()
 
 LANDKREIS = "lk_rosenheim"
 
-cache = {
-    LANDKREIS: {}
-}
+cache = {LANDKREIS: {}}
 
 
 @app.get("/" + LANDKREIS)
 async def blaue_tonne_dates(district: str) -> list[str]:
     PLANS = [
-        # {"url": "https://chiemgau-recycling.de/wp-content/uploads/2022/11/Abfuhrplan_LK_Rosenheim_2023.pdf", "pages": "1,2"},
-        # {"url": "https://chiemgau-recycling.de/wp-content/uploads/2023/11/Abfuhrplan_LK_Rosenheim_2024.pdf", "pages": "1,2"},
-        {"url": "https://chiemgau-recycling.de/wp-content/uploads/2025/01/Abfuhrplan_LK_Rosenheim_2025.pdf", "pages": "1,2"},
+        # {
+        #     "url": "https://chiemgau-recycling.de/wp-content/uploads/2022/11/Abfuhrplan_LK_Rosenheim_2023.pdf",
+        #     "pages": "1,2",
+        # },
+        # {
+        #     "url": "https://chiemgau-recycling.de/wp-content/uploads/2023/11/Abfuhrplan_LK_Rosenheim_2024.pdf",
+        #     "pages": "1,2",
+        # },
+        {
+            "url": "https://chiemgau-recycling.de/wp-content/uploads/2025/01/Abfuhrplan_LK_Rosenheim_2025.pdf",
+            "pages": "1,2",
+        },
     ]
 
     if district in cache[LANDKREIS]:
